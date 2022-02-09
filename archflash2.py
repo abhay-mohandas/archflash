92dfc16fbce2a0bd5c84da6a8c4f00ad2cf2151d
Abhay Mohandas <80393938+abhay-mohandas@users.noreply.github.com>
2022-02-09 11:38:46 +0530

    Add files via upload

---
+++ archflash2.py
@@ -0,0 +1,9 @@
+import os
+
+print("Setting up Locaton and Language information...")
+region=input("Enter the location region(Ex:Asia/Europe/America):")
+os.system("ls /usr/share/zoneinfo/"+region.capitalize())
+city=input("Enter the city name from the list(Ex:Kolkata):")
+os.system("ln -sf /usr/share/zoneinfo/"+region.capitalize()+"/"+city.capitalize()+" /etc/localtime")
+os.system("hwclock --systohc")
+os.system("locale-gen")
