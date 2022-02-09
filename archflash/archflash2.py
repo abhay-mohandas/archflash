bbf52b7e31a1f57a64a70cf8c9452d5241d78f27
Abhay Mohandas <80393938+abhay-mohandas@users.noreply.github.com>
2022-02-09 11:37:31 +0530

    Delete archflash2.py

--- archflash/archflash2.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print("Setting up Locaton and Language information...")
-region=input("Enter the location region(Ex:Asia/Europe/America):")
-os.system("ls /usr/share/zoneinfo/"+region.capitalize())
-city=input("Enter the city name from the list")
-os.system("ln -sf /usr/share/zoneinfo/"+region.capitalize()+"/"+city.capitalize()+" /etc/localtime")
-os.system("hwclock --systohc")
-os.system("locale-gen")
