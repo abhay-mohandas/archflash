65bb759846052d11b88a64f2875967913e25cc6f
Abhay Mohandas <80393938+abhay-mohandas@users.noreply.github.com>
2022-02-13 08:04:50 +0530

    Update archflash_next.py

--- archflash_next.py
+++ archflash_next.py
@@ -15,7 +15,7 @@ if region.capitalize() in zone_dir or region.upper()=="US":
 else:
     os.system("ln -sf /usr/share/zoneinfo/"+region.capitalize()+" /etc/localtime")
 os.system("clear")
-os.system("hwclock --systohc")
+os.system("hwclock --systohc --utc")
 print("Next step is to edit the locale file by uncommenting the necessary locales and saving the file...")
 input("Press Enter to continue")
 os.system("nano /etc/locale.gen")
@@ -34,11 +34,78 @@ if ans.lower()!="n":
     os.system("ls **/*.map.gz | grep "+key.lower()+" | less")
     keymap = input("Enter the layout name (Exclude the .map.gz extention)(Ex:us/uk/etc):")
     os.system('''echo "KEYMAP='''+keymap.lower()+'''" > /etc/vconsole.conf''')
+    os.system("cd")
     input("Press Enter to continue ")
 os.system("clear")
+parallel=input("Enable parallel downloads?(Y/n):")
+if parallel.lower()!="n":
+    parallel_num = input("Enter the number of parallel downloads to be enabled(2-7):")
+    if parallel_num > 7:
+        parallel_num = 7
+    elif parallel_num < 2:
+        parallel_num = 2
+    os.system('echo "ParallelDownloads='+parallel_num+'" > /etc/pacman.conf')
+os.system("clear")
+os.system("clear")
+print("Installing Reflector to update the mirror list...")
+os.system("pacman -S reflector")
+os.system("reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist")
+os.system("sudo systemctl enable reflector.service reflector.timer")
+os.system("clear")
+print("\nUpdating mirror list")
+os.system("sudo systemctl start reflector.service reflector.timer")
+os.system("clear")
 print("Installing necessary files and dependencies\n")
-os.system('''pacman -S --needed dhcpcd pacman-contrib archlinux-keyring base-devel usbutils lsof dialog \
+os.system('''pacman -S --needed dhcpcd pacman-contrib archlinux-keyring base-devel systemd usbutils lsof dialog \
                     zip unzip p7zip unrar lzop rsync traceroute bind-tools linux linux-headers \
-                    networkmanager openssh cronie xdg-user-dirs haveged''')
-
-
+                    networkmanager openssh cronie xdg-user-dirs haveged grub libinput dosfstools ntfs-3g btrfs-progs \
+                    exfat-utils gptfdisk fuse2 fuse3 fuseiso pulseaudio pulseaudio-alsa alsa-utils alsa-plugins \
+                    pulseausio-bluetooth pulseaudio-equilizer xorg-server xorg-xinit git''')
+vendor_name=['amd','intel','other']
+while True:
+    vendor=str(input("Enter the processor vendor name (AMD/Intel):"))
+    if vendor.lower() in vendor_name:
+        if vendor.lower() == 'other':
+            break
+        os.system("pacman -S --needed "+vendor.lower()+"-ucode")
+    else:
+        print("Invalid Option! Try again...(If your processor vendor is other than AMD and Intel, Enter 'other' as the vendor name)")
+os.system("clear")
+aur_list=["yay","paru","trizen","aura","none"]
+ans=input("Do you want to install an AUR helper(Ex:yay)?(Y/n):")
+y=1
+if ans.lower() != "n":
+    while True:
+        print("List of AUR helpers:")
+        for x in aur_list:
+            print(y+")"+x)
+        aur_helper = input("Enter the name of the AUR helper from the above list:")
+        if aur_helper.lower() in aur_list:
+            if aur_helper.lower() == "none":
+                break
+            os.system("git clone https://aur.archlinux.org/"+aur_helper+".git")
+            os.system("cd "+aur_helper)
+            os.system("makepkg -si")
+            os.system("cd")
+            os.system("rm -rf "+aur_helper)
+        else:
+            print("Invalid option! To cancel the AUR helper installation, enter 'none' ")
+os.system("clear")
+os.system("systemctl disable dhcpcd")
+os.system("systemctl enable sshd")
+os.system("systemctl enable cronie")
+os.system("systemctl enable NetworkManager")
+os.system("systemctl enable numlockon")
+print("Configuring GRUB...\n")
+os.system("mkdir /boot/grub")
+os.system("grub-mkconfig -o /boot/grub/grub.cfg")
+input("Press Enter to continue ")
+os.system("clear")
+print("Set the root password")
+os.system("passwd root")
+os.system("clear")
+account_name=input("Enter the user account name:")
+os.system("useradd -m "+account_name)
+print("Set the password for user account "+account_name)
+os.system("passwd "+account_name)
+os.system("exit")
