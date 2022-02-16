5a6e9cea259c5542de5e516c2e6f8d96a11380cf
Abhay Mohandas <80393938+abhay-mohandas@users.noreply.github.com>
2022-02-16 22:08:13 +0530

    Update archflash.py

--- archflash.py
+++ archflash.py
@@ -26,7 +26,7 @@ print("Devices:")
 os.system("fdisk -l")
 print("\nRAM installed:")
 os.system("lsmem | grep Total")
-temp=input("Continue?(Y/n):")
+temp=input("\nContinue?(Y/n):")
 if temp.lower()=="n":
     exit()
 os.system("clear")
@@ -35,41 +35,46 @@ print("""Next step will be of partitions...
 Recommended settings:   1Gb for boot (/boot)
                         6 to 10 Gb for Swap if RAM size is 8Gb(-/+2Gb the size of RAM installed) 
                         The rest of the storage for root (/)
+
                         
 Minimum settings:       500Mb for boot (/boot)
                         1-2Gb for swap (Values close to the RAM installed in the system) 
                         The rest of the storage for root (/)
 
 System Types:
-                /boot   : EFI System
-                swap    : Linux swap
-                /home   : Linux filesystem
+                /boot   : EFI System(If efi is available)
+                 swap   : Linux swap
+                /       : Linux filesystem
 
 Partitioning will be done manually in cfdisk.
 """)
 print("Caution! This program(at the moment) does not support separate /home partition\n")
 input("Enter to continue")
 os.system("cfdisk")
+print("Enter the partition names(Leave blank if none)")
 boot = input("Partition for boot(Ex: sda1):")
 swap = input("Partition for swap(Ex: sda2):")
 root = input("Partition for root(Ex: sda3):")
 print("Formatting the partitions...")
-os.system("mkfs.ext4 /dev/"+root)
-os.system("mkswap /dev/"+swap)
-os.system("mkfs.fat -F 32 /dev/"+boot)
-os.system("mount /dev/"+root+" /mnt")
-os.system("mkdir /mnt/boot")
-os.system("mount /dev/"+boot+" /mnt/boot")
-os.system("swapon /dev/"+swap)
+if boot != "":
+    os.system("mkfs.fat -F 32 /dev/"+boot)
+    os.system("mkdir /mnt/boot")
+    os.system("mount /dev/"+boot+" /mnt/boot")
+if swap != "":
+    os.system("mkswap /dev/"+swap)
+    os.system("swapon /dev/"+swap)
+if root != "":
+    os.system("mkfs.ext4 /dev/"+root)
+    os.system("mount /dev/"+root+" /mnt")
+
 print("\nBuilding the base system\n")
 os.system("pacstrap /mnt base linux linux-firmware dosfstools python nano less grep")
-os.system("genfstab -U /mnt >> /mnt/etc/fstab")
-os.system("cp /root/archflash/* /mnt/")
+os.system("genfstab -U -p /mnt >> /mnt/etc/fstab")
+os.system("cp /root/archflash/archflash_next.py /mnt")
 print("Mounting drives...")
 os.system("arch-chroot /mnt/ python archflash_next.py")
 os.system("clear")
-os.system("umount -R /mnt")
-os.system("swapoff /dev/vda2")
+print("Installation Complete!\n")
 ans = input("Reboot the system?(Y/n):")
 if ans.lower() != "n":
     os.system('reboot')
